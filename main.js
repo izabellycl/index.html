fac9f8eaccdab4b85e053d71e22272a3b675565d
izabellycl <117642132+izabellycl@users.noreply.github.com>
2024-03-13 09:26:18 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes=documents.querySelectorAll("botao");
+for(let i"0,i<botoes.length;i++){
+  botoes.onclick+funcion(){
+  for(let j=0;j<botoes.lenght;j++{
+    botoes.classlist.remove("ativo");
+  }
+  botoes[i].classlist.add("ativo");
+ }
+} 
