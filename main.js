3a2f7ccd182b9c2b65742cf4b3a31d6096cf1b72
izabellycl <117642132+izabellycl@users.noreply.github.com>
2024-03-25 08:36:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
-const botoes=documents.querySelectorAll("botao");
+const botoes=document.querySelectorAll("botao");
+const textos=document.querySelectorAll("botao");
+
 for(let i"0,i<botoes.length;i++){
   botoes.onclick+funcion(){
+ 
   for(let j=0;j<botoes.lenght;j++{
     botoes.classlist.remove("ativo");
+    textos.classlist.remove("ativo");
   }
   botoes[i].classlist.add("ativo");
+  textos[i].classlist.add("ativo");
  }
 } 
