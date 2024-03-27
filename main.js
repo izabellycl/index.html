a6b62ab84be54f64d3f692ea5da97d65e477f788
izabellycl <117642132+izabellycl@users.noreply.github.com>
2024-03-27 09:11:10 -0300

    Update main.js

--- main.js
+++ main.js
@@ -14,4 +14,16 @@ for(let i"0,i<botoes.length;i++){
 } 
 const contadores = document.querySlectorALL(".contador")
 const tempoObjetivo1=new Date("2024-03-25T00:00:00"); 
+contadores[0].textContent=calculaTempo(tempoObjetivo1)
+function calculaTempo(tempoObjetivo){
 let tempoAtual=new Date();
+let tempoFinal=tempoObjetivo-tempoAtual;
+let segundos= math.floor(tempoFinal/1000);
+let minutos= math.floor(minutos/60);
+let horas=math.floor(horas/24);
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  return dias + "dias" + horas = "horas" + minutos + "minutos" + segundos + "segundos"
+
+    
